29530c642a06cc38470d44f8f3b29e3afdd46426
admin <adviles.24@gmail.com>
2024-03-05 11:53:49 +0100

    first commit

---
+++ python-passgen.py
@@ -0,0 +1,10 @@
+import random # a dice roll function that gives the ability to create a random output given an input
+import string # this library gives more complexity when generating a password
+
+def generate_password(length: int = 10):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(alphabet) for i in range (length))
+    return password
+
+password = generate_password()
+print(f"Generated password: {password}")
